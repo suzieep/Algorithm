fba34b6971228d56a8af4d370250c6b6c9893fc4
Suzie Lee <wnsuwls0707@gmail.com>
2020-10-31 00:14:43 +0900

    2002 -

---
+++ week4/BOJ_2002.java
@@ -0,0 +1,37 @@
+package week4;
+
+import java.util.*;
+
+public class BOJ_2002 {
+
+    public static void main(String[] args) throws Exception{
+
+        Scanner scan = new Scanner(System.in);
+        int cars = scan.nextInt();
+        HashMap<String, Integer> carNames = new HashMap<>();
+        int count = 0;
+
+        for (int i = 0; i < cars; i++) {
+            String temp = scan.nextLine();
+            carNames.put(temp, i);
+        }
+        int[] out = new int[cars];
+
+        for (int i = 0; i < cars; i++) {
+            String temp = scan.nextLine();
+            out[i] = carNames.get(temp);
+        }
+
+        for (int i = 0; i < cars - 1; i++) {
+            for (int j = i + 1; j < cars; j++) {
+
+                if (out[i] > out[j]) {
+                    count++;
+                    break;
+                }
+
+            }
+        }
+        System.out.println(count);
+    }
+}
