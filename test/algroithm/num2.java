105ee14582e94fc17b82fbbd2b5bbe10bcc0d900
Suzie Lee <wnsuwls0707@gmail.com>
2020-11-02 18:58:13 +0900

    week4

---
+++ test/algroithm/num2.java
@@ -0,0 +1,22 @@
+package test.algroithm;
+
+import java.util.*;
+
+public class num2 {
+    public static String solution(String encrypted_text, String key, int rotation) {
+
+        
+        HashMap<String, Integer> carNames = new HashMap<>();
+
+        String answer = "";
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        String encrypted_text = "qyyigoptvfb";
+        String key ="abcdefghijk";
+        int rotation = 3;
+        
+        solution(encrypted_text,key, rotation);
+    }
+}
