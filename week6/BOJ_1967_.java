fd987b945fb89e6dbebe2ede9538f3c1db2c1b79
Suzie Lee <wnsuwls0707@gmail.com>
2020-11-13 18:46:50 +0900

    week6 submit

--- week6/BOJ_1967_.java
+++ week6/BOJ_1967_.java
@@ -8,31 +8,41 @@ public class BOJ_1967_ {
     static HashMap<Integer, Integer>[] hm = new HashMap[100001];
     static int[] list = new int[100001];
 
+
     public static int dfs(int v)  {
         Arrays.fill(visit, false);
-        Stack<Edge> s = new Stack<>();
-        max = 0;maxIndex = 1;s.push(new Edge(v, 0));
+        Stack<Box> s = new Stack<>();
+        max = 0;
+        maxIndex = 1;
+        s.push(new Box(v, 0));
         visit[v] = true;
         while (!s.isEmpty()) {
-            Edge curNode = s.pop();
+
+            Box curNode = s.pop();
             if (max < curNode.w) {
-                max = curNode.w; maxIndex = curNode.v;
+                max = curNode.w; 
+                maxIndex = curNode.v;
             }
+
             for (int key : hm[curNode.v].keySet()) {
-                Edge e = new Edge(key, hm[curNode.v].get(key));
+                Box e = new Box(key, hm[curNode.v].get(key));
                 if (!visit[e.v]) {
-                    visit[e.v] = true; s.push(new Edge(e.v, e.w + curNode.w));
+                    visit[e.v] = true; 
+                    s.push(new Box(e.v, e.w + curNode.w));
                 }
             }
         }
         return max;
     }
-    private static class Edge{
+
+    private static class Box{
         int v, w;
-        Edge(int v, int w) {
-            this.v = v; this.w = w;
+        Box(int v, int w) {
+            this.v = v; 
+            this.w = w;
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         n = sc.nextInt();
@@ -40,10 +50,14 @@ public class BOJ_1967_ {
             hm[i] = new HashMap<>();
         }
         for (int i = 1; i <= n - 1; i++) {
-            u = sc.nextInt();v = sc.nextInt();w = sc.nextInt();
-            hm[u].put(v, w);hm[v].put(u, w);
+            u = sc.nextInt();
+            v = sc.nextInt();
+            w = sc.nextInt();
+            hm[u].put(v, w);
+            hm[v].put(u, w);
         }
-        dfs(1);dfs(maxIndex);
+        dfs(1);
+        dfs(maxIndex);
         System.out.println(max);
     }
 
