6270e0f2711a36148804c2fcce25d9e53138f2c1
Suzie Lee <wnsuwls0707@gmail.com>
2021-01-31 05:20:47 +0900

    [Add] BOJ_1065

--- 공맹알/BOJ_1065.py
+++ 공맹알/BOJ_1065.py
@@ -1,2 +1,14 @@
-# BOJ 1065 한수 SILVER IV
+temp = 0
+_range = int(input())
+cnt = _range
 
+for i in range(1,_range+1):
+    in_list = str(i)
+    for j in range(len(in_list)-1):
+        now = int(in_list[j+1])-int(in_list[j])
+        if j != 0:
+            if temp!=now:
+                cnt -= 1
+        temp = now
+
+print(cnt)
