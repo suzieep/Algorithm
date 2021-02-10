cfe4c539515ab4cbc477e2d472784d2d7055a9ea
Suzie Lee <wnsuwls0707@gmail.com>
2021-02-11 00:22:56 +0900

    [Add] 4-1 상하좌우

--- 공맹알/3주차(구현)/상하좌우_4-1.py
+++ 공맹알/3주차(구현)/상하좌우_4-1.py
@@ -1,4 +1,20 @@
 n = int(input())
-arr = list(map(int,input().split()))
+arr = list(map(str,input().split()))
 
-direction = ['L', 'R', 'U', 'D']
+now = [1,1]
+
+for i in arr:
+    if i=='L':
+        if now[1]>1:
+            now[1]-=1
+    elif i=='R':
+        if now[1]<n:
+            now[1]+=1
+    elif i=='U':
+        if now[0]>1:
+            now[0]-=1
+    elif i=='D':
+        if now[1]<n:
+            now[0]+=1
+
+print(now[0],now[1])
