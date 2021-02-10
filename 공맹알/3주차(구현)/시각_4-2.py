e63d02e28bb3d8c2632f10345d564fe1a6550b58
Suzie Lee <wnsuwls0707@gmail.com>
2021-02-11 02:13:54 +0900

    [Add] 시각 4-2

---
+++ 공맹알/3주차(구현)/시각_4-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+cnt = 0
+for h in range(n+1):
+    for m in range(60):
+        for s in range(60):
+            time = str(h)+str(m)+str(s)
+            if '3' in time:
+                cnt+=1
+
+print(cnt)
+    
