dd61a621a27723591afc609fee8b5ea52647249d
Suzie Lee <wnsuwls0707@gmail.com>
2022-09-11 20:55:54 +0900

    kakao_비밀지도_오답.py

---
+++ 2022/0911/kakao_비밀지도_오답.py
@@ -0,0 +1,5 @@
+def solution(n, arr1, arr2):
+    answer = []
+    for i in range(n):
+        answer.append(bin(arr1[i]|arr2[i])[2:].rjust(n,"0").translate(str.maketrans('10', '# ')))
+    return answer
