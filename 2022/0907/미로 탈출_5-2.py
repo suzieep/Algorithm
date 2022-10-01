f0131d01d3f01adb2459e11d6aa1589ad2909332
Suzie Lee <wnsuwls0707@gmail.com>
2022-10-01 23:42:29 +0900

    chores

--- 2022/0907/미로 탈출_5-2.py
+++ 2022/0907/미로 탈출_5-2.py
@@ -1,8 +1,11 @@
 #23:57
+from collections import deque
 
 n,m = map(int,input().split())
 board=[]
 length_arr =[]
+dx = [1,0,-1,0]
+dy = [0,1,0,-1]
 for i in range(n):
     board.append(list(map(int,input().split())))
     length_arr.append([0 for _ in range(m)])
@@ -12,5 +15,10 @@ print(length_arr)
 #12:07
 
 def bfs():
+    q = deque()
+    length = 0
+    while q:
+        print(1)
+
     return 0
 
